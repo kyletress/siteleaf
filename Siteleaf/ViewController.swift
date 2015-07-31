3b72d44bc4258851a95fdcfb379bff98e01769d6
Kyle Tress <kyle@kyletress.com>
2015-07-31 18:29:59 -0400

    Add basic email/password login with basic authentication. Returns API key and secret from Siteleaf.

--- Siteleaf/ViewController.swift
+++ Siteleaf/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loginButton() {
+        // setup credentials
+        let loginString = "\(emailTextField.text):\(passwordTextField.text)"
+        let loginData = loginString.dataUsingEncoding(NSUTF8StringEncoding)
+        let base64login = loginData!.base64EncodedStringWithOptions(nil)
+        let authString = "Basic \(base64login)"
+        
+        // create the request and retrieve keys
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.siteleaf.com/v1/auth.json")!)
+        request.HTTPMethod = "POST"
+        
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        config.HTTPAdditionalHeaders = ["Authorization" : authString]
+        let session = NSURLSession(configuration: config)
+        
+        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            // println("Response: \(response)")
+            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("\(strData!)")
+            var err: NSError?
+            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
+            
+            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+            if(err != nil) {
+                println(err!.localizedDescription)
+                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("Error could not parse JSON: '\(jsonStr)'")
+            }
+            else {
+                // The JSONObjectWithData constructor didn't return an error. But, we should still
+                // check and make sure that json has a value using optional binding.
+                if let parseJSON = json {
+                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                    var success = parseJSON["success"] as? Int
+                    //println("Success: \(success)")
+                }
+                else {
+                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println("Error could not parse JSON: \(jsonStr)")
+                }
+            }
+        })
+        task.resume()
+    }
 }
-
