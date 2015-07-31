a3678df59cb6cd2300e79d64a0a73d8119e567ac
Kyle Tress <kyle@kyletress.com>
2015-07-31 16:19:15 -0400

    Initial Commit

---
+++ Siteleaf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Siteleaf
+//
+//  Created by Tress on 7/31/15.
+//  Copyright (c) 2015 Kyle Tress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
