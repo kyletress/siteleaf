1a4c6410b4cb7017bfabdea1b5f97993c9a31773
Kyle Tress <kyle@kyletress.com>
2015-08-01 02:20:39 -0400

    Add SwiftyJson, KeychainWrapper, and start work on the Router

--- Siteleaf/ViewController.swift
+++ Siteleaf/ViewController.swift
@@ -15,13 +15,12 @@ class ViewController: UIViewController {
   @IBOutlet weak var passwordTextField: UITextField!
   
   override func viewDidLoad() {
-      super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
+    super.viewDidLoad()
   }
 
   override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
   }
 
   @IBAction func loginButton() {
@@ -33,8 +32,21 @@ class ViewController: UIViewController {
       
       // create the request and retrieve keys
     Alamofire.request(.POST, "https://api.siteleaf.com/v1/auth.json", headers: headers)
-      .responseJSON { _, _, JSON, _ in
-        println(JSON)
+      .responseJSON { request, response, data, error in
+        var json = JSON(data!)
+        let key = json["api_key"]
+        let secret = json["api_secret"]
+        KeychainWrapper.setString("\(key)", forKey: "apiKey")
+        KeychainWrapper.setString("\(secret)", forKey: "apiSecret")
+        let retrievedKey: String? = KeychainWrapper.stringForKey("apiKey")
+        let retrievedSecret: String? = KeychainWrapper.stringForKey("apiSecret")
     }
+
+    Alamofire.request(.GET, "https://api.siteleaf.com/v1/ping.json", headers: headers)
+      .responseJSON { request, response, data, error in
+        var json = JSON(data!)
+        println(json)
+    }
+    
   }
 }
