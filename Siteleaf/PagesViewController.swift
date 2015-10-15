f1b8ce7b0ad9c9eb9226cb9f72cee8d4574f4a8c
Kyle Tress <kyle@kyletress.com>
2015-10-15 01:03:55 -0400

    Experiment with storyboard scenes for pages

---
+++ Siteleaf/PagesViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PagesViewController.swift
+//  Siteleaf
+//
+//  Created by Kyle Tress on 10/15/15.
+//  Copyright © 2015 Kyle Tress. All rights reserved.
+//
+
+import UIKit
+
+class PagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+    @IBAction func cancelToPlayersViewController(segue:UIStoryboardSegue) {
+    }
+  
+    @IBAction func savePlayerDetail(segue:UIStoryboardSegue) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
